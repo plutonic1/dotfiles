6e038efa08ae4613e8144ed76be05f36a7593f7e
plutonic <github@plutonic.tk>
2019-11-02 08:59:14 +0100

    added conky to installer

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 
 rm -rf $HOME/.dotfiles
 mkdir -p $HOME/.dotfiles
@@ -17,3 +17,12 @@ rm -f $HOME/.bashrc
 cp "$DIR/.bashrc" $HOME/.dotfiles/.bashrc
 ln -s -f $HOME/.dotfiles/.bashrc $HOME/.bashrc
 . $HOME/.bashrc
+
+rm -f $HOME/.conky_ips.sh
+cp "$DIR/.conky_ips.sh" $HOME/.dotfiles/.conky_ips.sh
+ln -s -f "$HOME/.dotfiles/.conky_ips.sh" $HOME/.conky_ips.sh
+chmod +x "$HOME/.dotfiles/.conky_ips.sh"
+
+rm -f $HOME/.conkyrc
+cp "$DIR/.conkyrc" $HOME/.dotfiles/.conkyrc
+ln -s -f "$HOME/.dotfiles/.conkyrc" $HOME/.conkyrc
