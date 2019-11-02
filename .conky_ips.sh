e7692919da944ad0ad75cd6815aeeb8cc5d683d9
plutonic <github@plutonic.tk>
2019-11-02 09:00:38 +0100

    ipv4 fix

--- .conky_ips.sh
+++ .conky_ips.sh
@@ -6,7 +6,7 @@ TYPE=$2
 ipv6_addresses=$(ip addr show dev eno1 | sed -e 's/^.*inet6 \([^ ]*\).*$/\1/;t;d')
 ipv4_address=$(ip addr show dev eno1 | sed -e 's/^.*inet \([^ ]*\).*$/\1/;t;d')
 
-echo "\${template1 $TYPE\ IPv4}       \${template3}\${addr $INTERFACE}"
+echo "\${template1 $TYPE\ IPv4}       \${template3}$ipv4_address"
 
 while read -r ipv6_address; do
     echo "\${template1 $TYPE\ IPv6}   \${template3}$ipv6_address"
